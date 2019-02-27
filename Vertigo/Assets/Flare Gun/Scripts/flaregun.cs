ba63bb0baff9e947001cdc713d5a185e81b0ee7f
Florine <32348261+Florine34@users.noreply.github.com>
2019-02-27 15:08:54 -0500

    Ajout fusil à peinture

---
+++ Vertigo/Assets/Flare Gun/Scripts/flaregun.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class flaregun : MonoBehaviour {
+	
+	public Rigidbody flareBullet;
+	public Transform barrelEnd;
+	public AudioClip flareShotSound;	
+	public int bulletSpeed = 2000;
+	
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+
+        if (Input.GetButtonDown("Fire1") && !GetComponent<Animation>().isPlaying)
+        {
+            Shoot();
+        }
+	}
+	
+	void Shoot()
+	{
+	    GetComponent<Animation>().CrossFade("Shoot");
+		GetComponent<AudioSource>().PlayOneShot(flareShotSound);
+        Rigidbody bulletInstance;			
+		bulletInstance = Instantiate(flareBullet,barrelEnd.position,barrelEnd.rotation) as Rigidbody; //INSTANTIATING THE FLARE PROJECTILE
+		bulletInstance.AddForce(barrelEnd.forward * bulletSpeed); //ADDING FORWARD FORCE TO THE FLARE PROJECTILE
+
+          
+	}
+}
