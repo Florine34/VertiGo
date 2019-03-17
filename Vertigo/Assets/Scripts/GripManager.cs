5fb6b78a5ad81464b66b6ad12af6aecf2c870bbb
Florine <32348261+Florine34@users.noreply.github.com>
2019-03-17 19:56:43 -0400

    Ajout mur escalade

---
+++ Vertigo/Assets/Scripts/GripManager.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GripManager : MonoBehaviour
+{
+
+    public Rigidbody Body;
+    public OVRTracker controller; 
+    public Pull left;
+    public Pull right;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        var lDevice = OVRInput.Get(OVRInput.Axis1D.PrimaryHandTrigger, OVRInput.Controller.LTouch);
+        var rDevice = OVRInput.Get(OVRInput.Axis1D.PrimaryHandTrigger, OVRInput.Controller.RTouch);
+
+        bool isGripped = left.canGrip || right.canGrip;
+
+        if (isGripped)
+        {
+            if (left.canGrip && lDevice > 0 )
+            {
+                Body.useGravity = false;
+                Body.isKinematic = true;
+                Body.transform.position += left.prevPos - left.transform.localPosition;
+            }
+            else if (left.canGrip && lDevice < 0)
+            {
+                Body.useGravity = true;
+                Body.isKinematic = false;
+                Body.velocity = (left.prevPos - left.transform.localPosition) / Time.deltaTime;
+            }
+
+            if (right.canGrip && rDevice > 0)
+            {
+                Body.useGravity = false;
+                Body.isKinematic = true;
+                Body.transform.position += right.prevPos - right.transform.localPosition;
+            }
+            else if (right.canGrip && rDevice < 0)
+            {
+                Body.useGravity = true;
+                Body.isKinematic = false;
+                Body.velocity = (right.prevPos - right.transform.localPosition) / Time.deltaTime;
+            }
+
+        }
+        else
+        {
+            Body.useGravity = true;
+            Body.isKinematic = false;
+        }
+
+     
+
+        left.prevPos = left.transform.localPosition;
+        right.prevPos = right.transform.localPosition;
+    }
+}
