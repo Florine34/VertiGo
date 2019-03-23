43cfae9f9dc1ca1c3e96f574240a123abe6d7048
hujkuk <uykuykuyk>
2019-03-23 16:35:25 -0400

    Teste VR avec fusil et line renderer

---
+++ Vertigo/Assets/AddRig.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddRig : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start() {
+        Rigidbody rig;
+        int n = transform.childCount;
+
+        for (int i = 0; i < n; i++) {
+            rig = transform.GetChild(i).gameObject.AddComponent(typeof(Rigidbody)) as Rigidbody;
+            rig.isKinematic = true;
+            rig.useGravity = false;
+        }
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+    }
+}
