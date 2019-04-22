292a89afb4c01112aad7a34d657b090a5aac8698
Bhastyen <bastien.duraj@live.fr>
2019-04-22 04:58:39 -0400

    Suppression terrain realistic + finalisation ascenseur.

---
+++ Vertigo/Assets/Scripts/TrapCom.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrapCom : MonoBehaviour {
+    public Rise elevator;
+
+    public void DisableTrap() {
+        elevator.DisableTrap();
+    }
+
+    public void EnabledTrap() {
+        elevator.EnabledTrap();
+    }
+}
